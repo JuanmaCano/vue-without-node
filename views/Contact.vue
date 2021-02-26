a1b9fd96d4c6bc657f780ce97ce5cdd3594c2991
Juanma Cano <juanmacano@gmail.com>
2021-02-26 19:23:08 +0100

    vuetable in Contact view (not works)

--- views/Contact.vue
+++ views/Contact.vue
@@ -1,17 +1,29 @@
 <template>
 	<div>
-		<p>{{ msg }}</p>
+		<vuetable
+			ref="vuetable"
+			api-url="https://vuetable.ratiw.net/api/users"
+			:fields="['name', 'nickname', 'email', 'gender']"
+			data-path=""
+			pagination-path=""
+		></vuetable>
 	</div>
 </template>
 
 <script>
-module.exports = {
-	name: 'Contact',
-
-	data: function () {
-		return {
-			msg: 'Contact Form',
-		};
-	},
-};
+	/*
+vue.js:634 [Vue warn]: 
+Unknown custom element: 
+<vuetable> - did you register the component correctly?
+For recursive components, make sure to provide the "name" option.
+found in
+---> <Contact>
+       <Root>
+*/
+	module.exports = {
+		name: 'Contact',
+		components: {
+			//Vuetable,
+		},
+	};
 </script>
